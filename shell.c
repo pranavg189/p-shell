2fb547e7fa524e3ee3802ed8228b23d0e046f962
Pranav Ganorkar <pranavg189@gmail.com>
2016-02-02 01:39:30 +0530

    Added code for reading a line

--- shell.c
+++ shell.c
@@ -42,5 +42,31 @@ char *shell_read_line(void)
 		exit(EXIT_FAILURE);
 
 	}
+
+	while(1){
+		// Read a character
+		c = getchar();
+
+		// If we hit EOF, replace it with a null character and return.
+		if(c == EOF || c == '\n') {
+			buffer[position] = '\0';
+			return buffer;
+		} else {
+			buffer[position]=c;
+		}
+		position++;
+
+		// If we have exceeded buffer, reallocate
+
+		if(position >= bufsize) {
+			bufsize += LSH_RL_BUFSIZE;
+			buffer = realloc(buffer, bufsize);
+			if(!buffer) {
+				fprintf(stderr, "lsh: allocation error\n");
+				exit(EXIT_FAILURE);
+			}
+		}
+	}
 }
+
 
