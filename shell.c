05a4745f4f78884736f0a992c818c87661e75807
Pranav Ganorkar <pranavg189@gmail.com>
2016-02-07 03:53:51 +0530

    implemented shell_launch() to launch a program given in the arguments

--- shell.c
+++ shell.c
@@ -105,5 +105,30 @@ char **shell_split_line(char *line)
 	return tokens;
 }
 
+/* Function to launch a program given in the arguments */
+int shell_launch(char **args)
+{
+	pid_t pid, wpid;
+	int status;
+
+	pid = fork();		/* Duplicate the process */
+	if(pid == 0){
+		// Child process
+		if(execvp(args[0], args) == -1) {
+			perror("lsh");
+		}
+		exit(EXIT_FAILURE);
+	} else if (pid < 0){
+		// Error forking
+		perror("lsh");
+	} else {
+		// Parent process
+		do {
+			wpid = waitpid(pid, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
+	return 1;
+}
+
 
 
